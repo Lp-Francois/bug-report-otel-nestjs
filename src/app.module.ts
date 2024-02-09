a47ef6f15a0b041a7f953e2b5d6b12bba778beae
Francois Le Pape <francois@ninetailed.com>
2024-02-09 15:26:08 +0100

    improve logging of trace

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,23 @@ import { LoggerModule } from 'nestjs-pino';
       {
         level: 'debug',
         base: undefined, // removes pid and hostname
-        // transport: { 
-        //   target: 'pino-pretty', 
-        //   options: {
-        //     colorize: true,
-        //     messageFormat: '{traceId}'
-        //   },
-        // },
+
+        transport: { 
+          target: 'pino-pretty', 
+          options: {
+            colorize: true,
+            ignore: "pid,hostname,level,time",
+            translateTime: false,
+            singleLine: false,
+            hideObject: true,
+            messageFormat: "[Nest] {if traceId}[trace={traceId}]{end}{if context}[{context}] {end} {msg}"
+            // messageFormat: (log, messageKey, levelLabel) => {
+            //   if (log?.traceId) return `${messageKey} + ${log.traceId}`
+            //   // do some log message customization
+            //   return messageKey;
+            // }
+          },
+        },
       }
   })],
   controllers: [AppController],
